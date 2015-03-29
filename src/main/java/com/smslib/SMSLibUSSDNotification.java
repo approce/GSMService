97f5e5b5ed1fcb9ca5520a15e3127a395de83489
Roman Zayats <seateq64@gmail.com>
2015-03-29 16:08:53 +0300

    Create ussd notification

---
+++ src/main/java/com/smslib/SMSLibUSSDNotification.java
@@ -0,0 +1,27 @@
+package com.smslib;
+
+import com.utils.xml.StringMethods;
+import org.ajwcc.pduUtils.gsm3040.PduUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smslib.AGateway;
+import org.smslib.IUSSDNotification;
+import org.smslib.USSDResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SMSLibUSSDNotification implements IUSSDNotification {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SMSLibUSSDNotification.class);
+
+    @Override
+    public void process(AGateway aGateway, USSDResponse ussdResponse) {
+        try {
+            String body = StringMethods.find(ussdResponse.getRawResponse(), "\"", "\"");
+            body = PduUtils.decodeUcs2Encoding(null, PduUtils.pduToBytes(body));
+            LOG.debug("USSD response received gateway id: {}\n{}", aGateway.getGatewayId(), body);
+        } catch (Exception e) {
+            LOG.error("Exception while USSD find body {}", e);
+        }
+    }
+}
