f3cacc9b551d40fd32ce1182d9977444dd3e0372
Roman Zayats <seateq64@gmail.com>
2015-05-04 19:58:12 +0300

    Create provider model.

--- src/main/java/com/model/SIMProvider.java
+++ src/main/java/com/model/SIMProvider.java
@@ -1,14 +1,28 @@
 package com.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sim_provider")
 public class SIMProvider {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "provider_id")
     public final Integer ID;
 
-    //TODO use hibernate
+    @Column(name = "name")
     public final String NAME;
 
+    @Column(name = "get_number_ussd")
     public final String GET_NUMBER_COMMAND;
 
+    @Column(name = "description")
     public final String DESCRIPTION;
 
     public SIMProvider(Integer ID, String NAME, String GET_NUMBER_COMMAND, String DESCRIPTION) {
@@ -17,5 +31,4 @@ public class SIMProvider {
         this.GET_NUMBER_COMMAND = GET_NUMBER_COMMAND;
         this.DESCRIPTION = DESCRIPTION;
     }
-
 }
