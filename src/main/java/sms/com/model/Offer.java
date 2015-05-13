b157aaadd5b29b9e1b792a6e9ab928fb1bcda5b9
approce <seateq64@gmail.com>
2015-05-13 08:56:02 +0300

    Add getters/setters to Offer, override equals.

--- src/main/java/sms/com/model/Offer.java
+++ src/main/java/sms/com/model/Offer.java
@@ -28,4 +28,52 @@ public class Offer {
     @Column(name = "priority")
     private Double priority;
 
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getShort_name() {
+        return short_name;
+    }
+
+    public void setShort_name(String short_name) {
+        this.short_name = short_name;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getOriginator() {
+        return originator;
+    }
+
+    public void setOriginator(String originator) {
+        this.originator = originator;
+    }
+
+    public Double getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Double priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Offer offer = (Offer) obj;
+        return this.getShort_name().equals(offer.getShort_name());
+    }
 }
