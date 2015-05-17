b60feb5a0e8701204be67883a78dad3c73a8a789
approce <seateq64@gmail.com>
2015-05-17 14:15:50 +0300

    Refactor Repo's.

--- src/main/java/sms/com/repository/MessageRepository.java
+++ src/main/java/sms/com/repository/MessageRepository.java
@@ -1,9 +1,7 @@
 package sms.com.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import sms.com.model.Message;
 
-@Repository
 public interface MessageRepository extends CrudRepository<Message, Long> {
 }
--- src/main/java/sms/com/repository/RequestRepository.java
+++ src/main/java/sms/com/repository/RequestRepository.java
@@ -1,9 +1,7 @@
 package sms.com.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import sms.com.model.Request;
 
-@Repository
 public interface RequestRepository extends CrudRepository<Request, Long> {
 }
